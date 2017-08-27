7e58ed4ccbeb6fdd3b9b68f48677f0b757854de7
Julian Rubin <rubin94@gmail.com>
2017-08-27 23:27:26 +0200

    Podstawowa obsługa żądań do aplikacji

---
+++ src/main/java/lesziy/carol/controller/ApiController.java
@@ -0,0 +1,13 @@
+package lesziy.carol.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @RequestMapping("/api")
+    public Integer wotPage() {
+        return 2;
+    }
+}
