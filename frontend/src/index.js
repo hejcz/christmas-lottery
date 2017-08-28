136b7597fc027477b7778f74f9a746c1129ad7a5
Julian Rubin <rubin94@gmail.com>
2017-08-28 07:17:40 +0200

    Konfiguracja środowiska deweloperskiego Reacta i przykładowy AJAX

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+fetch('/api')
+    .then(resp => resp.json())
+    .then(json => console.log(json));
+
 ReactDOM.render(<App />, document.getElementById('root'));
