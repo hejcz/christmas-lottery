a23860810a7af50423a833f93e20fad4051c49d1
Lesziy <przemyslawsaramonowicz@gmail.com>
2017-08-31 01:25:04 +0200

    jakaś tam obsługa logowania na froncie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import $ from 'jquery';
+import Login from './Login';
+import Page from './Page'
+import 'jquery/dist/jquery.min';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const loginFormId = "login-form";
+//sadly only fix for fckng ES6
+window.jQuery = window.$ = $;
+require('bootstrap');
 
 class App extends Component {
 
-  handleLogin(e) {
-      e.preventDefault();
-      // wywala się bo nie znamy numeru sesji. Można zrobić endpoint session, który
-      // zwróci sesję.
-      fetch('/api/login',
-          {method: 'POST',
-              body: new FormData(document.getElementById(loginFormId)),
-              credentials: 'include'})
-          .then(resp => {
-              return resp.json();
-          })
-          .then(json => console.log(json));
+  constructor() {
+    super();
+    this.state = {loggedIn: false};
   }
 
   pingToApi() {
-      fetch(document.getElementById("api-path").value, {method: 'POST', credentials: 'include'})
-          .then(resp => {
-              return resp.json();
-          })
-          .then(json => console.log(json));
+    fetch($("#api-path").val(), {method: 'POST', credentials: 'include'})
+      .then(resp => {
+        return resp.json();
+      })
+      .then(json => console.log(json));
   }
-
+  loggedInSuccessfully() {
+    this.setState ({loggedIn: true});
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const loggedIn = this.state.loggedIn;
+    let page = null;
+    if (loggedIn) {
+      page = <Page/>;
+    } else {
+      page = <Login action={this.loggedInSuccessfully.bind(this)}/>;
+    }
 
-        <form id={loginFormId}>
-            <label htmlFor="username">User Name:</label>
-            <input id="username" name="username" type="text"/>
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="password"/>
-            <input type="submit" onClick={this.handleLogin} value="Log In"/>
-        </form>
-
-        <input id="api-path" name="api" type="text"/>
-        <button onClick={this.pingToApi} style={{width: 20, height: 20}} label="Click" />
-
+    return(
+      <div className="mainApp">
+        {page}
       </div>
     );
+
   }
 }
-
 export default App;
