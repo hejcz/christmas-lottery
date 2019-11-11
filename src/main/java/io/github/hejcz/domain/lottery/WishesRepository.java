e62a1423028984a842df3d5bbe4c1787da95a3ea
hejcz <rubin94@gmail.com>
2019-11-11 12:18:16 +0100

    Fix wishes table name in query

--- src/main/java/io/github/hejcz/domain/lottery/WishesRepository.java
+++ src/main/java/io/github/hejcz/domain/lottery/WishesRepository.java
@@ -1,18 +1,18 @@
 package io.github.hejcz.domain.lottery;
 
-import io.github.hejcz.domain.user.DbUser;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @Repository
 interface WishesRepository extends CrudRepository<DbWish, Integer> {
 
-    @Query("UPDATE wishes SET locked = FALSE")
+    @Modifying
+    @Query("UPDATE DbWish SET locked = FALSE")
     void unlockAllWishes();
 
     Collection<DbWish> findByRecipientId(Integer recipient);
